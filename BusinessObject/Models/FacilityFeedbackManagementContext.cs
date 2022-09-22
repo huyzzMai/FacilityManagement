15e8fe1672481434c9a2fef143335fb82cefcd1b
phucvhd <75359238+phucvhd@users.noreply.github.com>
2022-09-22 16:50:01 +0700

    init folders

--- BusinessObject/Models/FacilityFeedbackManagementContext.cs
+++ BusinessObject/Models/FacilityFeedbackManagementContext.cs
@@ -30,7 +30,7 @@ namespace BusinessObject.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =(local); database = FacilityFeedbackManagement;uid=sa;pwd=1234567890;");
+                optionsBuilder.UseSqlServer("server =localhost; database = FacilityFeedbackManagement;uid=sa;pwd=Sa123456;");
             }
         }
 
